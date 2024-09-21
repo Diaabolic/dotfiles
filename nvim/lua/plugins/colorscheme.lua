0c798ee0bb31dd1b00215556ae4968792839b11e
NawafAllahyani <diabolic@NawafAllahyani.local>
2024-09-21 23:42:28 +0300

    Changes!

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,10 @@
 return {
   "craftzdog/solarized-osaka.nvim",
+  "catppuccin/nvim",
+  "tokyonight.nvim",
+  "ellisonleao/gruvbox.nvim",
+  "EdenEast/nightfox.nvim",
+  "rebelot/kanagawa.nvim",
   lazy = true,
   priority = 1000,
   opts = function()
@@ -7,4 +12,10 @@ return {
       transparent = true,
     }
   end,
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -62,6 +62,12 @@ return {
         },
       })
       vim.treesitter.language.register("markdown", "mdx")
+
+      -- Key mappings for resizing splits
+      vim.api.nvim_set_keymap("n", "<C-Up>", ":resize +2<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<C-Down>", ":resize -2<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<C-Left>", ":vertical resize -2<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<C-Right>", ":vertical resize +2<CR>", { noremap = true, silent = true })
     end,
   },
 }
