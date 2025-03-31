43316f375412ac25064000ba9105891c71f37a02
Diaabolic <nawaf.lahyani11@gmail.com>
2025-04-01 02:42:45 +0300

    changed lazyvim to diabolic in ui

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -66,15 +66,12 @@ return {
     },
   },
 
-  -- animations
   {
-    "echasnovski/mini.animate",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      opts.scroll = {
-        enable = false,
-      }
-    end,
+    "snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+    },
+    keys = {},
   },
 
   -- buffer line
@@ -94,36 +91,22 @@ return {
       },
     },
   },
-
-  -- filename
+  -- statusline
   {
-    "b0o/incline.nvim",
-    dependencies = { "craftzdog/solarized-osaka.nvim" },
-    event = "BufReadPre",
-    priority = 1200,
-    config = function()
-      local colors = require("solarized-osaka.colors").setup()
-      require("incline").setup({
-        highlight = {
-          groups = {
-            InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
-            InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
-          },
-        },
-        window = { margin = { vertical = 0, horizontal = 1 } },
-        hide = {
-          cursorline = true,
-        },
-        render = function(props)
-          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-          if vim.bo[props.buf].modified then
-            filename = "[+] " .. filename
-          end
-
-          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-          return { { icon, guifg = color }, { " " }, { filename } }
-        end,
-      })
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      local LazyVim = require("lazyvim.util")
+      opts.sections.lualine_c[4] = {
+        LazyVim.lualine.pretty_path({
+          length = 0,
+          relative = "cwd",
+          modified_hl = "MatchParen",
+          directory_hl = "",
+          filename_hl = "Bold",
+          modified_sign = "",
+          readonly_icon = " 󰌾 ",
+        }),
+      }
     end,
   },
 
@@ -141,20 +124,25 @@ return {
   },
 
   {
-    "nvimdev/dashboard-nvim",
-    event = "VimEnter",
-    opts = function(_, opts)
-      local logo = [[
+    "MeanderingProgrammer/render-markdown.nvim",
+    enabled = false,
+  },
+
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[
 ██████╗ ██╗ █████╗ ██████╗  ██████╗ ██╗     ██╗ ██████╗
 ██╔══██╗██║██╔══██╗██╔══██╗██╔═══██╗██║     ██║██╔════╝
 ██║  ██║██║███████║██████╔╝██║   ██║██║     ██║██║     
 ██║  ██║██║██╔══██║██╔══██╗██║   ██║██║     ██║██║     
 ██████╔╝██║██║  ██║██████╔╝╚██████╔╝███████╗██║╚██████╗
 ╚═════╝ ╚═╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚══════╝╚═╝ ╚═════
-      ]]
-
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
-      --opts.config.header = vim.split(logo, "\n")
-    end,
+   ]],
+        },
+      },
+    },
   },
 }
